394c19f5c8fb6656337f835a7659d7ff98daf6a2
Yu-Sung Su <suyusung@tsinghua.edu.cn>
2009-01-30 08:03:31 +0000

    <empty commit message>

--- pkg/R/sigma.hat.R
+++ pkg/R/sigma.hat.R
@@ -1,7 +1,6 @@
 setMethod("sigma.hat", signature(object = "lm"),
     function(object)
     {
-    object.class <- class(object)[[1]]
     sigma <- summary(object)$sigma
     return (sigma)
     }
@@ -17,7 +16,6 @@ setMethod("sigma.hat", signature(object = "glm"),
                   else{
                     object$dispersion
                   }
-    object.class <- class(object)[[1]]
     if (object$family$family == "gaussian") {
       sigma <- sqrt(dispersion)
     }
