78617642942dac4c5eb74274fb421ed7ffd63c7d
Lionel Henry <lionel.hry@gmail.com>
2015-01-22 22:37:12 +0100

    Roxygenize extractDIC.R

--- pkg/R/extractDIC.R
+++ pkg/R/extractDIC.R
@@ -1,12 +1,29 @@
-
-
+#' Extract AIC and DIC from a mer model
+#'
+#' Computes the (generalized) Akaike *A*n *I*nformation *C*riterion
+#' and *D*eviance *I*nformation *C*riterion for a mer model.
+#'
+#' @param fit fitted \code{merMod} mode, usually the result of a
+#'   fitter like \code{merMod}.
+#' @param ... further arguments (currently unused).
+#'
+#' @author
+#'   Andrew Gelman \email{gelman@@stat.columbia.edu};
+#'   Yu-Sung Su \email{suyusung@@tsinghua.edu.cn}
+#' @keywords manip methods
+#' @export
+#' @examples
+#' fm1 <- lmer(Reaction ~ Days + (Days|Subject), sleepstudy)
+#' extractAIC(fm1)
+#' extractDIC(fm1)
 extractDIC <- function(fit,...){
   UseMethod("extractDIC")
 }
 
-
+#' @rdname extractDIC
+#' @export
 extractDIC.merMod <- function(fit,...){
-        #REML <- fit@dims["REML"]
+#        REML <- fit@dims["REML"]
 #        llik <- logLik(fit, REML)
 #        dev <- fit@deviance["ML"]
 #        n <- fit@dims["n"]
@@ -29,7 +46,8 @@ extractDIC.merMod <- function(fit,...){
 
 
 
-#
+# #' @rdname extractDIC
+# #' @export
 #extractAIC.mer <- function(fit,...){
 ##     REML <- fit@dims["REML"]
 ##    llik <- logLik(fit, REML)
